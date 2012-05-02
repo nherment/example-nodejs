d241f949fe6c433dc6a458c3effebeb1a8814f37
Nicolas Herment <nherment@gmail.com>
2012-05-01 17:22:14 -0700

    initial commit. simple and stupid test

---
+++ test/test.js
@@ -0,0 +1,13 @@
+var f = require("../index.js")
+
+var should = require("should")
+
+describe("When testing", function() {
+
+  it("a hello world", function() {
+
+    f().should.equal("Hello World")
+
+  })
+
+})
